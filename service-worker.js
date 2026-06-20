630081a6ebd3e8ba0eadb89ed3d149604c832ee6
ryon787-afk <ryon787@gmail.com>
2026-06-20 19:07:41 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='pan-print-v2';
-const ASSETS=['./','index.html','style.css','app.js','manifest.json','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE='pan-print-multi-v1';
+const FILES=['./','index.html','manifest.json','service-worker.js','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
