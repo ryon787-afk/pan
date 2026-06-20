3cebd420d1cfe685c5f1b7a8552c7beb610ed998
ryon787-afk <ryon787@gmail.com>
2026-06-20 21:26:47 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='kubochan-pan-v3.3.0';
-const ASSETS=['./','index.html?v=3.3.0','style.css?v=3.3.0','storage.js?v=3.3.0','app.js?v=3.3.0','manifest.json?v=3.3.0','icon-192.png','icon-512.png'];
+const CACHE_NAME='kubochan-pan-v3.3.1';
+const ASSETS=['./','index.html?v=3.3.1','style.css?v=3.3.1','storage.js?v=3.3.1','app.js?v=3.3.1','manifest.json?v=3.3.1','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
