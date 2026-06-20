5ad7ebc6294edb11e25259f28256ba7d3848c81a
ryon787-afk <ryon787@gmail.com>
2026-06-20 23:50:15 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='kubochan-pan-v339';
-const ASSETS=['./index.html?v=338','./manifest.json?v=338','./icon-192.png','./icon-512.png'];
+const CACHE='kubochan-pan-v3310';
+const ASSETS=['./index.html?v=3310','./manifest.json?v=3310','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))})
