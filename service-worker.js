200e59f09dc3a68238a7c142fa2c315472095c91
ryon787-afk <ryon787@gmail.com>
2026-06-22 01:01:17 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='kubochan-pan-v40101';
-const ASSETS=['./','./index.html?v=400','./manifest.json?v=400','./icon-192.png','./icon-512.png'];
+const CACHE='kubochan-pan-v40104';
+const ASSETS=['./','./index.html?v=404','./manifest.json?v=404','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=400'))))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=404'))))});
