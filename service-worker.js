edd5b25bbdb79973ef158f0d42ea06f587f58c0e
ryon787-afk <ryon787@gmail.com>
2026-06-23 00:31:48 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='kubochan-pan-v4024';
-const ASSETS=['./index.html?v=424','./manifest.json?v=424','./icon-192.png','./icon-512.png'];
+const CACHE='kubochan-pan-v4023';
+const ASSETS=['./index.html?v=423','./manifest.json?v=423','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
@@ -7,7 +7,7 @@ self.addEventListener('fetch',e=>{
   if(req.method!=='GET')return;
   const accept=req.headers.get('accept')||'';
   if(req.mode==='navigate'||accept.includes('text/html')){
-    e.respondWith(fetch(req,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html?v=424'))));
+    e.respondWith(fetch(req,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html?v=423'))));
     return;
   }
   e.respondWith(caches.match(req).then(r=>r||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res})));
